971ef0ee2618ccf421c57e0c5feddab012b3902a
Sindharta Tanuwijaya <sindharta@users.noreply.github.com>
2025-11-17 18:33:36 +0900

    refactor: cleanup shadertemplate and shaderblock files (#575)

--- Scripts/clean_up_text.sh
+++ Scripts/clean_up_text.sh
@@ -1,6 +1,6 @@
 # replace tabs
-git ls-files -z '*.cs' '*.md' '*.shader' '*.cginc' '*.hlsl' | xargs -0 sed -Ei 's/\t/    /g'
+git ls-files -z '*.cs' '*.md' '*.shader' '*.cginc' '*.hlsl' '*.shadertemplate' '*.shaderblock'| xargs -0 sed -Ei 's/\t/    /g'
 
 # remove UTF-8 BOM, and trailing spaces or tabs at the end of lines
-git ls-files -z '*.cs' '*.md' '*.shader' '*.cginc' '*.hlsl' | xargs -0 sed -Ei 's/\r//g;s/\s+$//;s/\xef\xbb\xbf//g'
+git ls-files -z '*.cs' '*.md' '*.shader' '*.cginc' '*.hlsl' '*.shadertemplate' '*.shaderblock'| xargs -0 sed -Ei 's/\r//g;s/\s+$//;s/\xef\xbb\xbf//g'
 
